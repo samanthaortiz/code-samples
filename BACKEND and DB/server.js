7b5ca533cd71565b9f8b61f9ba117cb39c77e2e1
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-11-27 14:00:21 -0500

    README.md added. server added. renamed actionsAndReducers to rootReducer

---
+++ BACKEND and DB/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./dist'));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+var apiRouter = require("./routes/routes.js");
+app.use("/api", apiRouter);
+
+app.use('/', function (req, res){
+  res.sendFile(path.resolve('client/index.html'));
+})
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, function(error){
+  if(error) throw error;
+  console.log('Express server listening on port', port);
+});
